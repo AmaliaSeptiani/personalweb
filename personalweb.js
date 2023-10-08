7fcd90d995d33431fb3a725e7aa34f31a88594e4
AmaliaSeptiani <amalia2003septiani@students.unnes.ac.id>
2023-10-08 10:16:49 +0700

    create header sticky navbar

--- personalweb.js
+++ personalweb.js
@@ -15,4 +15,7 @@ window.onscroll = () =>{
             })
         };
     });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 };
